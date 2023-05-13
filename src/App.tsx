074c47308c49e482738f4735ce07ca84e72b8e7e
GretioRumuat14 <s21910147@student.unklab.ac.idgit config --global user.email s21910147@student.unklab.ac.idgit config --global user.email s21910147@student.unklab.ac.id>
2023-05-13 21:40:23 +0800

    first commit for the CoffeShop project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SplashScreen } from "./pages";
+
+const App = () => {
+  return (
+    <View>
+      <SplashScreen/>
+    </View>
+  )
+}
+
+export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import BgSplashScreen from './coffeimage.png';
+
+export {
+    BgSplashScreen
+}
